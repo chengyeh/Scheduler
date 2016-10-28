1b50e932261aa762f6ce1f516322ae7b19cf9374
Cheng Yeh Lee <chengyeh@ku.edu>
2016-10-28 14:18:36 -0500

    Set condition for scheduling PSJF

---
+++ doc/html/structjob__t.js
@@ -0,0 +1,9 @@
+var structjob__t =
+[
+    [ "arrival_time", "structjob__t.html#ace7ceffac2a843361204c4d9ba709bc8", null ],
+    [ "job_number", "structjob__t.html#acab2869f53a44b411ecb3009a7424280", null ],
+    [ "priority", "structjob__t.html#a173228d1697d71307f6538b2914f7156", null ],
+    [ "remaining_time", "structjob__t.html#a7eb75e48c9c3388898ae752dc5427602", null ],
+    [ "running_time", "structjob__t.html#a710d2b8b8d506de75276806d768f4ce8", null ],
+    [ "start_time", "structjob__t.html#ae95337ab719891172746b1dd7eddb067", null ]
+];
